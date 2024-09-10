56ffe641b6d2e4c0e3bd69a9679ffd4dbb505b31
Alissonf94 <alissonfernandesxx55@gmail.com>
2024-09-10 18:52:19 -0300

    feat: merge

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Products } from "./components/templates/Products";
+import Login from "./components/templates/Login";
 
 export function Router(){
 	return(
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Products />} />
+				<Route path= "/login" element = {<Login/>}/>
 			</Routes>
 		</BrowserRouter>
 	);
