e52ba3228608b82423f534739013cb7a7f3a4e08
Alissonf94 <alissonfernandesxx55@gmail.com>
2024-09-11 16:11:01 -0300

    feat: ffunctional filter

--- src/components/atoms/Filter.tsx
+++ src/components/atoms/Filter.tsx
@@ -1,10 +1,14 @@
-export default function Filter (){
+interface FilterProps {
+    onFilterChange: (value: string) => void;
+}
+export default function Filter ({onFilterChange}: FilterProps){
     return(
-        <select >
+        <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5" onChange={(e)=> onFilterChange(e.target.value)}>
+            <option selected value="todos">Todos</option>
             <option value="maiorPreco">Maior preço</option>
-            <option value="menorPreco">Meno preço </option>
-            <option value="maiorQuantidade">Menor quantidade </option>
-            <option value="menosQuantidade">Maior quantidade</option>
+            <option value="menorPreco">Menor preço </option>
+            <option value="menorQuantidade">Menor quantidade </option>
+            <option value="maiorQuantidade">Maior quantidade</option>
             <option value="maisAvaliado">Maior avaliação</option>
             <option value="menosAvaliado">Menor Avaliação</option>
         </select>
--- src/components/templates/Products.tsx
+++ src/components/templates/Products.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Filter from "../atoms/Filter";
 import { Title } from "../atoms/Title";
 import { Header } from "../organisms/Header";
@@ -5,15 +6,16 @@ import { ProductList } from "../organisms/ProductList";
 import Sidebar from "../organisms/Sidebar";
 
 export function Products(){
+	const [filter, setFilter] = useState<string>("");
 	return(
 		<>
 			<Header/>
 			<main className="lg:w-4/5 mx-auto flex flex-col gap-8">
 				<div className="flex justify-between">
 					<Title content="Produtos"/>
-					<Filter/>
+					<Filter onFilterChange={setFilter}/>
 				</div>
-				<ProductList />
+				<ProductList filter={filter}/>
 				<Sidebar/>
 			</main>
 		</>
