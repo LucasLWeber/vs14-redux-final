b5e71c513129a13e96c724bfcf7414bce6da2465
Edson Machado <edsonfernandes.n@outlook.com>
2024-09-11 10:35:41 -0300

    sidebar com upload de imagem

--- src/components/templates/Products.tsx
+++ src/components/templates/Products.tsx
@@ -1,11 +1,14 @@
 import { Title } from "../atoms/Title";
 import { ProductList } from "../organisms/ProductList";
+import Sidebar from "../organisms/Sidebar";
 
 export function Products(){
 	return(
 		<main className="lg:w-4/5 mx-auto flex flex-col gap-y-4">
 			<Title content="Produtos"/>
 			<ProductList />
+			<Sidebar/>
+
 		</main>
 	);
 }
