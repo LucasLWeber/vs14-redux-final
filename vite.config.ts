5b567d014bd59f6d23e8fc7e091aa6b090b61244
Edson Fernandes <118990742+EdsonMFN@users.noreply.github.com>
2024-09-12 14:15:44 -0300

    Update vite.config.ts
    vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,3 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://sandbox.melhorenvio.com.br',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-})
