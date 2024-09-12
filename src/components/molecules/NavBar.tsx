ddf068cd30343b30cd113dd2bd8aa081826367e7
Alissonf94 <alissonfernandesxx55@gmail.com>
2024-09-12 12:42:10 -0300

    feat: atualização do navBar

--- src/components/molecules/NavBar.tsx
+++ src/components/molecules/NavBar.tsx
@@ -5,19 +5,18 @@ import { CheckoutModal } from "../organisms/CheckoutModal";
 
 export function Navbar() {
   const [isModalOpen, setModalOpen] = useState(false); 
-  const [counter, setCounter] = useState<number>(0);
 
   const toggleModal = () => {
     setModalOpen(!isModalOpen);
   };
 
   return (
-    <nav className="flex itens-center">
+    <nav>
       <ul className="flex items-center justify-center gap-x-8">
         <div className="relative">
           <button onClick={toggleModal}>
             <Icon url="/assets/Cart.svg" alt="Carrinho de compras" heigth="29" width="29" />
-            <CartCounter counter={counter} />
+            <CartCounter />
           </button>
         </div>
       </ul>
